09102b9492390f4ac5e2540461b50fc00196f671
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-03-26 18:50:40 +0100

    Added body of the home page

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
+import "../css/footer.css";
 import group from "../assets/images/Group.png";
 import instagram_black from "../assets/images/Instagram.png";
 import twitter_black from "../assets/images/Twitter.png";
@@ -7,14 +8,29 @@ import facebook_black from "../assets/images/FACEBOOK.png";
 
 const Footer = () => {
   return (
-    <Row sm={12} md={4}>
-      <Col>
-        <img src={group} alt="" />
+    <Row className="footercontainer">
+      <Col sm={12} md={4}>
+        <div>
+          <img src={group} alt="" className="footergroup" />
+          <div className="iamgegroup">
+            <img src={instagram_black} alt="" className="footerimage" />
+            <img src={facebook_black} alt="" className="footerimage" />
+            <img src={twitter_black} alt="" className="footerimage" />
+          </div>
+        </div>
       </Col>
-      <Col>
-        <img src={instagram_black} alt="" />
-        <img src={facebook_black} alt="" />
-        <img src={twitter_black} alt="" />
+      <Col sm={12} md={4}>
+        <h6 className="ibadan">Ibadan</h6>
+        <h6 className="address">
+          Kuye Street, Ikolaba Estate, Adjacent A3 Estate, Ikolaba, Ibadan
+        </h6>
+      </Col>
+      <Col sm={12} md={4}>
+        <h6 className="ibadan">Our Mission</h6>
+        <h6 className="address">
+          Opportunity to work with achieve over and beyond their set targets.
+          Our clients’ growth and satisfaction is our growth and fulfilment.{" "}
+        </h6>
       </Col>
     </Row>
   );
